9926894eaffb96ba0261438b86e5d6ed099404cb
DaekiKim <daeki56@gmail.com>
2021-01-10 00:00:12 +0900

    웹 뷰로 간단한 웹 브라우저 만들기

--- Web/ViewController.swift
+++ Web/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     @IBOutlet var myWebView: WKWebView!
     @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
     
+    // url의 인수를 통해 웹 페이지의 주소를 전달받아 웹 페이지를 보여 줌
     func loadWebPage(_ url: String) {
         let myUrl = URL(string: url)
         let myRequest = URLRequest(url: myUrl!)
@@ -23,7 +24,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         myWebView.navigationDelegate = self
-        loadWebPage("http://2sam.net")
+        loadWebPage("http://2sam.net").   // 앱 실행 시 초기 홈페이지를 불러옴
     }
     
     func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
@@ -41,24 +42,51 @@ class ViewController: UIViewController, WKNavigationDelegate {
         myActivityIndicator.isHidden = true
     }
 
-    @IBAction func btnGotoUrl(_ sender: UIButton) {
-    }
-    @IBAction func btnGotoSite1(_ sender: UIButton) {
-    }
-    @IBAction func btnGotoSite2(_ sender: UIButton) {
-    }
-    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
-    }
-    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+    // "http://" 문자열이 없을 경우 자동으로 삽입
+    func checkUrl(_ url: String) -> String {
+        var strUrl = url
+        let flag = strUrl.hasPrefix("http://")
+        if !flag {
+            strUrl = "http://" + strUrl
+        }
+        return strUrl
     }
     
+    // 텍스트 필드에 적힌 주소로 웹 뷰 로딩
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+        let myUrl = checkUrl(txtUrl.text!)
+        txtUrl.text = " "
+        loadWebPage(myUrl)
+    }
+    
+    // Site1 버튼 클릭 시
+    @IBAction func btnGotoSite1(_ sender: UIButton) {
+        loadWebPage("https://www.naver.com")
+    }
+    @IBAction func btnGotoSite2(_ sender: UIButton) {
+        loadWebPage("https://github.com/Kimdaeki97/Web")
+    }
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+        let htmlString = "<h1> HTML String </h1><p> String 변수를 이용한 웹 페이지 </p><p><a href=\"http://2sam.net\">2sam</a>으로 이동</p>"
+        myWebView.loadHTMLString(htmlString, baseURL: nil)
+    }
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+        let filePath = Bundle.main.path(forResource: "htmlView", ofType: "html")
+        let myUrl = URL(fileURLWithPath: filePath!)
+        let myRequest = URLRequest(url: myUrl)
+        myWebView.load(myRequest)
+    }
     @IBAction func btnStop(_ sender: UIBarButtonItem) {
+        myWebView.stopLoading()
     }
     @IBAction func btnReload(_ sender: UIBarButtonItem) {
+        myWebView.reload()
     }
     @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+        myWebView.goBack()
     }
     @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+        myWebView.goForward()
     }
     
 }
